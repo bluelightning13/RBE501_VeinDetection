211537676c9d72e2c0257925e5ba0e4233bb8d44
bluelightning13 <mehiatt@wpi.edu>
2017-04-17 19:26:32 -0400

    first
    needs work

---
+++ code.py
@@ -0,0 +1,304 @@
+import cv2
+import numpy
+from random import randint
+from matplotlib import pyplot as plt
+from matplotlib import pylab as cm
+import RPi.GPIO as GPIO
+import os
+import glob
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(7, GPIO.OUT)
+GPIO.setup(11, GPIO.OUT)
+GPIO.setup(12, GPIO.OUT)
+
+GPIO.setup(15, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
+
+GPIO.output(11, 1)
+GPIO.output(12, 1)
+GPIO.output(15, 1)
+GPIO.output(16, 1)
+GPIO.output(21, 1)
+GPIO.output(22, 1)
+
+def calibration():
+    # termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((5*6,3), np.float32)
+    objp[:,:2] = np.mgrid[0:6,0:5].T.reshape(-1,2)
+    # Arrays to store object points and image points from all the images.
+    objpoints = [] # 3d point in real world space
+    imgpoints1 = [] # 2d points in image plane.
+    imgpoints2 = [] # 2d points in image plane.
+    images1 = glob.glob('c1/*jpg')
+    images2 = glob.glob('c2/*jpg')
+
+    lenImg = len(images1)
+
+    for fname in range(0,lenImg):
+        img1 = cv2.imread(images1[fname])
+        img1 = cv2.resize(img1,(0,0),fx=.25, fy=.25)
+        img2 = cv2.imread(images2[fname])
+        img2 = cv2.resize(img2,(0,0),fx=.25, fy=.25)
+
+        print"print grey"
+        gray1 = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
+        gray2 = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+
+        # Find the chess board corners
+        ret1, corners1 = cv2.findChessboardCorners(gray1, (5,6))#,flags = cv2.CALIB_CB_FAST_CHECK)
+        ret2, corners2 = cv2.findChessboardCorners(gray2, (5,6))
+        # If found, add object points, image points (after refining them)
+        if ret1 and ret2:
+            objpoints.append(objp)
+            corners1 = cv2.cornerSubPix(gray1,corners1,(11,11),(-1,-1),criteria)
+            imgpoints1.append(corners1)
+            corners2 = cv2.cornerSubPix(gray2,corners2,(11,11),(-1,-1),criteria)
+            imgpoints2.append(corners2)
+
+            #Draw and display the corners
+            #img = cv2.drawChessboardCorners(img, (5,6), corners,ret)
+            #cv2.imshow('img',img)
+            #cv2.waitKey()
+
+    gray = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
+    imgsize = gray.shape
+    #ensure that at least three points are found - this can be increased for
+    #more accurate calibration
+    if(len(objpoints)>3):
+        print "Images calibrated successfully"
+        #print imgsize
+        #cv2.destroyAllWindows()
+        ret1, mtx1, dist1, rvecs1, tvecs1 = cv2.calibrateCamera(objpoints, imgpoints1, imgsize, None, None)
+        ret2, mtx2, dist2, rvecs2, tvecs2 = cv2.calibrateCamera(objpoints, imgpoints2, imgsize, None, None)
+        return r1,r2,p1,p2,q
+    else:
+        print "Please calibrate again!"
+
+    #StereoCalibration: inputs:object points, img1 points, img2 points, image size,term_crit, flags
+    #output: camM1, distCoeffs1, camM2,disCoeffs2, R, T, E, F
+    #cm1, dist1, cm2, dist2, R, T, E, F = cv2.stereoCalibration(objpoints, imgpoints1, imgpoints2, imgsize)
+    #r1, r2, p1, p2, q = cv2.stereoRectify(cm1,cm2,dist1,dist2,imgsize, R, T)
+
+
+
+
+def filterImg(r1,r2,p1,p2,q):
+    imgL = cv2.imread('c1/capture_1.jpg',0)
+    imgR = cv2.imread('c2/capture_1.jpg',0)
+
+    stereo = cv2.createStereoBM(numDisparities=16,blockSize=15)
+    disparity = stereo.compute(imgL, imgR)
+
+    #Median Blur
+    imgL = cv2.medianBlur(imgL,9)
+    imgR = cv2.medianBlur(imgR,9)
+
+    filterImg = cv2.imread('filter2.png', 0)
+    imgL = cv2.resize(imgL, (0,0), fx = .79, fy = .79)
+    imgR = cv2.resize(imgR, (0,0), fx = .79, fy = .79)
+    filterImg = cv2.resize(filterImg,(0,0), fx = 1, fy=.75)
+
+    print "starting dft..."
+
+    dft_shiftL = numpy.fft.fftshift(cv2.dft(numpy.float32(imgL), flags = cv2.DFT_COMPLEX_OUTPUT))
+    dft_shiftR = numpy.fft.fftshift(cv2.dft(numpy.float32(imgR), flags = cv2.DFT_COMPLEX_OUTPUT))
+
+    rows, cols = imgL.shape
+    crow, ccol = rows/2, cols/2
+    mask = numpy.zeros((rows, cols,2), numpy.uint8)
+    #filterImg = filterImg[484:1564,64:1984]
+    mask[:,:,0] = filterImg[:,:]
+    mask[:,:,1] = filterImg[:,:]
+
+
+    img_backL = cv2.idft(numpy.fft.ifftshift(dft_shiftL*mask))
+    img_backR = cv2.idft(numpy.fft.ifftshift(dft_shiftR*mask))
+
+    img_backR = cv2.magnitude(img_backR[:,:,0], img_backR[:,:,1])
+    img_backR = cv2.normalize(img_backR, 0, 255, cv2.NORM_MINMAX)
+    img_backL = cv2.magnitude(img_backL[:,:,0], img_backL[:,:,1])
+    img_backL = cv2.normalize(img_backL, 0, 255, cv2.NORM_MINMAX)
+
+    print "thresholding..."
+    retR,img_threshR = cv2.threshold(img_backR,.135,255,cv2.THRESH_BINARY)
+    retL,img_threshL = cv2.threshold(img_backL,.135,255,cv2.THRESH_BINARY)
+    #cv2.namedWindow('thresholding', cv2.WINDOW_NORMAL)
+    #cv2.resizeWindow('thresholding',cols/2, rows/2)
+    #cv2.imshow("thresholding", img_thresh)
+
+
+    print "e and d..."
+    eltC = cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3))
+    eltR = cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
+    eroR = cv2.dilate(img_threshR,eltC, iterations=10)
+    diaR = cv2.erode(eroR,eltR, iterations=30)
+    diaR = cv2.dilate(diaR,eltR,iterations=15)
+
+    eroL = cv2.dilate(img_threshL,eltC, iterations=10)
+    diaL = cv2.erode(ero,eltRL, iterations=30)
+    diaL = cv2.dilate(dia,eltRL,iterations=15)
+
+    #cv2.namedWindow('DilationErosion', cv2.WINDOW_NORMAL)
+    #cv2.resizeWindow('DilationErosion',cols/2, rows/2)
+    #cv2.imshow("DilationErosion", dia)
+    mask = numpy.zeros((rows+2,cols+2), numpy.uint8)
+    #flood fill the corners
+    cv2.floodFill(diaR, mask, (0,0), 255)
+    cv2.floodFill(diaR, mask, (2047, 1535), 255)
+    cv2.floodFill(diaR, mask, (0, 1535), 255)
+    cv2.floodFill(diaR, mask, (2047, 0), 255)
+
+    cv2.floodFill(diaL, mask, (0,0), 255)
+    cv2.floodFill(diaL, mask, (2047, 1535), 255)
+    cv2.floodFill(diaL, mask, (0, 1535), 255)
+    cv2.floodFill(diaL, mask, (2047, 0), 255)
+    print "starting line detection..."
+    #crop and do region
+    cpL = diaL
+    cpR = diaR
+    print "Non-Zero, %d", cv2.countNonZero(cpL)
+    cpR = 255 - cpR
+    cpL = 255 - cpL
+    print "Inversed, %d", cv2.countNonZero(cpR)
+
+    size = numpy.size(cpL)
+    skelR = numpy.zeros(cpR.shape,numpy.float32) #uint8
+    skelL = numpy.zeros(cpL.shape,numpy.float32) #uint8
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
+    finished = False
+
+    print "Starting skeletonization"
+    while(not finished):
+        eroded = cv2.erode(cpL,kernel)
+        temp = cv2.dilate(eroded,kernel)
+        temp = cv2.subtract(cpL,temp)
+        skel = cv2.bitwise_or(skel,temp)
+        cpL = eroded.copy()
+
+        #zeros = size - cv2.countNonZero(img)
+        #print zeros
+        print cv2.countNonZero(cpL)
+        if cv2.countNonZero(cpL)<10:
+            finished = True
+
+    finished = False
+    while(not finished):
+        eroded = cv2.erode(cpR,kernel)
+        temp = cv2.dilate(eroded,kernel)
+        temp = cv2.subtract(cpR,temp)
+        skel = cv2.bitwise_or(skel,temp)
+        cpR = eroded.copy()
+
+        #zeros = size - cv2.countNonZero(img)
+        #print zeros
+        print cv2.countNonZero(cpR)
+        if cv2.countNonZero(cpR)<10:
+            finished = True
+
+
+    veinL = skelL
+    veinR = skelR
+    #vein = vein.astype(numpy.uint8)
+    #over = cv2.addWeighted(img, .5, vein, .5, 0.0)
+    #cv2.namedWindow('Vein', cv2.WINDOW_NORMAL)
+    #cv2.resizeWindow('Vein',cols/2, rows/2)
+    #cv2.imshow("Vein", vein)
+    #cv2.waitKey()
+
+    #if the x are out of tolerance, highlight next section to indicate curve
+    points = cv2.findNonZero(vein)
+    points = cv2.findNonZero(vein)
+    #change skel to rgb
+    colorImg = cv2.cvtColor(skel, cv2.COLOR_GRAY2RGB)
+    randClr = [0,0,0]
+    rangepts = points.shape[0]
+    a = points[0,0][0]
+    clr = 0
+
+    for i in range(0,rangepts):
+        xpt = points[i,0][0]
+        ypt = points[i,0][1]
+        if (xpt > 1+a or xpt < a-1):
+            #change color
+            print "change"
+            randClr = [clr+10, 0, 200]
+            #randClr = [randint(10, 225),randint(10, 225),randint(10, 225)]
+            a = points[i,0][0]
+        colorImg[ypt,xpt] = randClr
+    #figure out camera calibrations
+    ori = cv2.imread('capture_1.jpg')
+
+    colorImg = colorImg.astype(numpy.uint8)
+    over = cv2.addWeighted(ori, .5, colorImg, .5, 0.0)
+    #rectify here
+
+
+    points = Q*[x,y,disparity(x,y),1] #might need to tranpose Q
+    finalPoint = [points[0]/points[3],points[1]/points[3],points[2]/points[3]]
+    return finalPoint
+
+
+
+
+
+
+
+
+def photoCapture(a):
+    GPIO.output(7,0)
+    GPIO.output(11,0)
+    GPIO.output(12,1)
+    capture(1, a)
+
+    GPIO.output(7,0)
+    GPIO.output(11,1) #here
+    GPIO.output(12,0)
+    capture(2, a)
+
+
+def capture(folder, cam):
+    cmd = "raspistill -t 500 -o c_%d/capture_%d.jpg" % folder, cam
+    os.system(cmd)
+
+def photoCaptureClibration():
+    photoCapture(5)
+    print "Press enter to continue to next image (1/5)"
+    cv.waitKey()
+    photoCapture(6)
+    print "Press enter to continue to next image (2/5)"
+    cv.waitKey()
+    photoCapture(7)
+    print "Press enter to continue to next image (3/5)"
+    cv.waitKey()
+    photoCapture(8)
+    print "Press enter to continue to next image (4/5)"
+    cv.waitKey()
+    photoCapture(9)
+    print "Press enter to continue to next image (5/5)"
+    cv.waitKey()
+
+
+
+if __name__ == "__main__":
+    #capture an image
+    print "Calibration starting. Please have caibration image ready..."
+    photoCaptureClibration()
+    #calibrate device
+
+    r1,r2,p1,p2,q = calibration()
+    print "Waiting for users. Press a key to continue."
+    cv2.waitKey()
+    photoCapture(1,1)
+    finalpoints = filterImg(r1,r2,p1,p2,q):
+
+    GPIO.output(7,0)
+    GPIO.output(11,0)
+    GPIO.output(12,1)
